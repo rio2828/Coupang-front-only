ebc01ae9c3a6459da6fd1395e35731bf5ba59d1c
KimTaeHyun <eddiekim1023@gmail.com>
2022-08-11 21:39:56 +0900

    반 완성.이름변경 + 탭바 삽입 전

---
+++ CoupangOnlyFront2/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  CoupangOnlyFront2
+//
+//  Created by 김태현 on 2022/08/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet weak var searchController: UISearchBar!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            
+        updateUI()
+        
+        
+    }
+
+    private func updateUI() {
+        
+
+        }
+        
+
+    
+    
+}
+
+
